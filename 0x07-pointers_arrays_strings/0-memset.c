e60b2a66fe7650d56349a1f6461759f60fe9291b
Melvinoh <99516217+Melvinoh@users.noreply.github.com>
2022-03-29 03:21:39 +0300

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,21 @@
-#include main.h
+#include "main.h"
+
 /**
- * _memeset: function that fill n bytes of memory area pointed to by s
- * @b consonant byte 
- * @*s variable holding memory spacce 
+ * *_memset - fills memory with a constant byte
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
+ *
+ * Return: pointer to the memory area s
  */
- 
-char *memset(char *s, char b, unsigned int  n)
+char *_memset(char *s, char b, unsigned int n)
 {
-	int i;
-	for (i = 0, i < n, i++)
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}
+
 	return (s);
 }
-	
-
