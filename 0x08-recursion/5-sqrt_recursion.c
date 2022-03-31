5cbcc13d0e632aa4002f02702afc27a35be44701
melvinoh <murichumelvin@gmailcom>
2022-03-31 02:04:36 +0000

    recursion in c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * sqrt_check - checks for the square root of c
+ * @g: guess at sqrt
+ * @c: number to find sqrt of
+ *
+ * Return: -1 or sqrt of c
+ */
+int sqrt_check(int n, int m)
+{
+	if (n * n == c)
+		return (g);
+	if (n * n > c)
+		return (-1);
+	return (sqrt_check(g + 1, c));
+}
+
