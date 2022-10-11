499e534dfca52256dcbd3466dd0b3d628be29e5a
melvinoh <murichumelvin@gmail.com>
2022-10-12 04:18:22 +0500

    c static libraries

--- 0x09-static_libraries/holberton.h
+++ 0x09-static_libraries/holberton.h
@@ -1,9 +1,9 @@
-#ifndef MAIN_H
-#define MAIN-H 
+#ifndef FILE_HOLBERTON
+#define FILE_HOLBERTON
 
 int _putchar(char c);
 int _islower(int c);
-int _is alpha(int c);
+int _isalpha(int c);
 int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
@@ -12,15 +12,14 @@ void _puts(char *s);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
-char *strncat(char *dest, char *src, int n);
-char *strncpy(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s char c);
-unsigned int _strspn(char *s char *accept);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
-char *strstr(char *haystack, char *needle);
+char *_strstr(char *haystack, char *needle);
 
 #endif
-
