2a04861784c898e7f93cae471c9fd1b81fc250cd
Melvinoh <99516217+Melvinoh@users.noreply.github.com>
2024-01-19 18:38:22 +0300

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,20 +1,20 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- * get_bit - returns the value of a bit at an index in a decimal number
- * @n: number to search
- * @index: index of the bit
- *
- * Return: value of the bit
+ * get_bit - return one bit
+ * @n: the number
+ * @index: the position to read the bit
+ * Description: return 1 bit
+ * section header: the header of this function is holberton.h
+ * Return: the value of the bit
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int bit_val;
+	unsigned long int bit;
 
-	if (index > 63)
+	if (index > 64)
 		return (-1);
 
-	bit_val = (n >> index) & 1;
+	bit = n >> index;
 
-	return (bit_val);
+	return (bit & 0x1);
 }
